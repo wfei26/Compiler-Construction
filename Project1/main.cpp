e950429391802d1b42cc879d21ebff604ffcf43b
Wei Fei <weifei6441@gmail.com>
2018-01-13 22:08:38 -0800

    Add files via upload

---
+++ Project1/main.cpp
@@ -0,0 +1,17 @@
+/***************************************************************************
+*Name: Wei Fei
+*KUID: 2538810
+*Date: 09/26/15
+*File: main.cpp
+*Course: EECS665 - Compiler Construction
+*Project: Assignment 1: nfa2dfa - Subset construction to convert NFA to DFA
+****************************************************************************/
+
+#include "Executive.h"
+
+int main()
+{
+    Executive myExe;
+    myExe.printFinalOutput();
+    return 0;
+}
